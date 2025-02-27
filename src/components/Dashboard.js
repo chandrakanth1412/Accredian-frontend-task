e7fc6afc0adc99168f8ea2e5248edf2f7ac22a04
chandrakanth1412 <nuthanagantichandrakanth58@gmail.com>
2025-02-27 11:30:06 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
 
   useEffect(() => {
     // Fetch referrals from backend
-    axios.get("https://accredian-backend-task-u5c5.onrender.com/api/referrals")
+axios.get("https://accredian-backend-task-u5c5.onrender.com/api/referrals")
 
       .then(response => {
         setReferrals(response.data); // Store referrals in state
